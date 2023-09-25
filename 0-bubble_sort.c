a1a59dd0fae776f7e70abc5c967679b110757053
SAMAymen <samoudiayman777@gmail.com>
2023-09-25 18:29:02 +0100

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,15 +9,18 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
+
 	for (i = 0; i < size; i++)
 	{
 		int swapped = 0;
 		size_t j;
+
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				int temp = array[j];
+                
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
