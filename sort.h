9a27c14fe5cdc15a2b5aa7b3720a93e4c8c22dce
SAMAymen <samoudiayman777@outlook.com>
2023-09-25 21:30:41 +0100

    done

--- sort.h
+++ sort.h
@@ -6,6 +6,7 @@
 
 /* Comparison direction macros for bitonic sort */
 #define UP 0
+#define DOWN 1
 
 /**
  * enum bool - Enumeration of Boolean values.
@@ -44,5 +45,10 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
